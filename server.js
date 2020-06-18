00b3f541f92b1c7baec20874e0b1a95248f49e8e
amireskandar <amirsobhylabib7@gmail.com>
2020-06-18 22:57:18 +0200

    version 1.0.0

--- server.js
+++ server.js
@@ -21,14 +21,16 @@ mongoose
 //Use routes
 app.use('/api/items', items);
 
-// serve static assets if in production
-if (process.env.NODE_ENV === 'PRODUCTION') {
+// Serve static assets if in production
+if (process.env.NODE_ENV === 'production') {
+  // Set static folder
   app.use(express.static('client/build'));
+
   app.get('*', (req, res) => {
     res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
-  })
-}
-const port = process.env.PORT || 5000;
-app.listen(5000, () => {
-  console.log(`Server is running on port: ${port}`);
-});
+  });
+
+  const port = process.env.PORT || 5000;
+  app.listen(5000, () => {
+    console.log(`Server is running on port: ${port}`);
+  });
